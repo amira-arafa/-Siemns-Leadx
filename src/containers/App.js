ad95097017ba95386fdcb8b980ccf7e0cf09ded0
amira7 <amiraarafa7@gmail.com>
2021-09-28 21:08:32 +0200

    make app component functional component instead of class component

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,37 +1,25 @@
-import React, { Component } from 'react';
-import { Router } from 'react-router-dom';
-import history from '../routes/History';
-import Routes from '../routes/Routes';
-import { connect } from 'react-redux';
-import { IntlProvider } from 'react-intl';
-import messages from '../assets/Local/messages';
-import './App.scss';
+import React from "react";
+import { Router } from "react-router-dom";
+import { useSelector } from "react-redux";
+import history from "../routes/History";
+import Routes from "../routes/Routes";
+import { IntlProvider } from "react-intl";
+import messages from "../assets/Local/messages";
+import "./App.scss";
 
-class App extends Component {
+const App = () => {
+  const { locale } = useSelector((state) => state);
+  const { lang } = locale;
+  return (
+    <IntlProvider locale={lang} messages={messages[lang]}>
+      <div
+        className={lang === "ar" ? "rtl" : "ltr"}
+        dir={lang === "ar" ? "rtl" : "ltr"}
+      >
+        <Router history={history}>{Routes}</Router>
+      </div>
+    </IntlProvider>
+  );
+};
 
-  render() {     
-    const { lang } = this.props;
-    console.log(lang);
-    
-    return (
-      <IntlProvider
-        locale={lang}
-        messages={messages[lang]}>
-        <div className={lang === 'ar' ? 'rtl' : 'ltr'} dir={lang === 'ar' ? 'rtl' : 'ltr'}>
-          <Router history={history}>
-            {Routes}   
-          </Router>
-        </div>
-      </IntlProvider>
-    );
-  }
-}
-
-const mapStateToProps = state => {
-  console.log(state);
-  return {
-    lang: state.locale.lang
-  }
-}
-
-export default connect(mapStateToProps)(App);
+export default App;
