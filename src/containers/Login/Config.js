aadea4083376b620c9eb09c887ce5221a07a15fd
amira7 <amiraarafa7@gmail.com>
2022-01-05 20:34:15 +0200

    changing baseurl of apis

--- src/containers/Login/Config.js
+++ src/containers/Login/Config.js
@@ -2,6 +2,6 @@ export const config = {
     clientId: "fc2e272f-56b5-4466-ad0b-206119970ad5",
     redirectUri:  window?.location?.href?.includes("localhost")
     ? "http://localhost:3000"
-    : "https://siemens-leadx.netlify.app",
+    : "siemenshealthneer-leadx.com",
     postLogoutRedirectUri : 'https://localhost:3000',
 };
